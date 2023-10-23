3a97b6dafc522b371c96313efb27d7239d4f662b
Guilherme Teixeira Santos <41587365+guiteixeira-io@users.noreply.github.com>
2023-10-22 23:54:04 -0300

    Initial Charge
    Initial Charge

--- bin/server.js
+++ bin/server.js
@@ -10,18 +10,18 @@ app.set('port', port);
 const server = http.createServer(app);
 
 server.listen(port);
-server.on('error' , onError);
-server.on('listening' , onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 console.log('API rodando na porta ' + port);
 
-function normalizePort(val){
+function normalizePort(val) {
     const port = parseInt(val, 10);
 
     if (isNaN(port)) {
         return val;
     }
 
-    if (port >= 0 ){
+    if (port >= 0) {
         return port;
     }
 
@@ -29,28 +29,26 @@ function normalizePort(val){
 }
 
 function onError(error) {
-    if (error.syncall !== 'listen'){
+    if (error.syncall !== 'listen') {
         throw error;
     }
 
     const bind = typeof port === 'string' ?
-    'Pipe ' + port :
-    'Port ' + port;
+        'Pipe ' + port :
+        'Port ' + port;
 
     switch (error.code) {
         case 'EACCES':
             console.error(bind + ' requires elevated privileges');
             process.exit(1);
             break;
-        case 'EADDRINUSE' :
+        case 'EADDRINUSE':
             console.error(bind + ' is already in use');
             process.exit(1);
             break;
         default:
             throw error;
     }
-
-
 }
 
 function onListening() {
--- src/app.js
+++ src/app.js
@@ -1,29 +1,38 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (red, res, next) => {
-    res.status(200).send({
-        tittle: "Node Store API",
-        version: "0.0.1"
-    });
-});
+//Conecta ao banco
+mongoose.connect('mongodb+srv://root:FZHu86Yu1y63fHp7@cluster-inicial.vuhvv7c.mongodb.net/?retryWrites=true&w=majority')
 
-const auth = '45720513809:45720513809';
+//CArrega os Models
+const Product = require('./models/product');
 
+//Carrega as Rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
 
-route.get('/consulta', (req, res) => {
-    http.get({
-       url: 'http://168.138.231.9:10666/get-token', username:'45720513809', password:'45720513809'
-    },(error,res,body) => {
-        console.log(body);
-    });
-    
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', route);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
+
+//const auth = '45720513809:45720513809';
+
+
+// route.get('/consulta', (req, res) => {
+//     http.get({
+//         url: 'http://168.138.231.9:10666/get-token', username: '45720513809', password: '45720513809'
+//     }, (error, res, body) => {
+//         console.log(body);
+//     });
+
+// });
