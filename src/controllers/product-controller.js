833f3584487e22665767187aa79086c8edf82f93
Guilherme Teixeira Santos <41587365+guiteixeira-io@users.noreply.github.com>
2023-10-23 01:16:36 -0300

    CRUD Complete
    CRUD Complete

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -5,7 +5,45 @@ const Product = mongoose.model('Product');
 
 exports.get = (req, res, next) => {
     Product
-        .find({})
+        .find({
+            active: true
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product
+        .findOne({
+            slug: req.params.slug,
+            active: true
+        }, 'title description price slug tags')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getById = (req, res, next) => {
+    Product
+        .findById(req.params.id)
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
+exports.getByTag = (req, res, next) => {
+    Product
+        .find({
+            tags: req.params.tag,
+            active: true
+        })
         .then(data => {
             res.status(200).send(data);
         }).catch(e => {
@@ -31,13 +69,37 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
+    Product
+    .findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            slug: req.body.slug
+        }
+    }).then(x => {
+        res.status(200).send({
+            message: 'Produto atualizado com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao atualizar produto!',
+            data: e
+        });
     });
 };
 
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Product
+    .findOneAndRemove(req.body.id,)
+    .then(x => {
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao remover produto!',
+            data: e
+        });
+    });
 };
